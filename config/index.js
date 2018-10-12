ca30f626f841b2098062f4152416ff61a840860b
tianmeng <tianmeng1@jd.com>
2018-10-12 12:13:14 +0800

    init

---
+++ config/index.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+  templates: require('./templates'),
+};
---
+++ config/templates.js
@@ -0,0 +1,13 @@
+/**
+ * smarter init 命令下的模板库列表配置
+ *
+ * @prop {string} name 给你的脚手架起一个好听的名字
+ * @prop {string} git 模板库的 git 地址
+ * @prop {string} desc 一句话说清楚你的脚手架是干什么的
+ */
+
+module.exports = [{
+  name: 'vue-template',
+  git: 'https://github.com/pspgbhu/Rephic.git',
+  desc: '支持微联本地调试的 vue 模板',
+}];
