b5935268fc6f9d47aa6b8cbf2b422c353728b4db
tianmeng <tianmeng1@jd.com>
2018-10-13 20:58:40 +0800

    feat: add templates info

--- config/templates.js
+++ config/templates.js
@@ -6,8 +6,69 @@
  * @prop {string} desc 一句话说清楚你的脚手架是干什么的
  */
 
-module.exports = [{
-  name: 'devtool-vue-template',
-  git: 'https://github.com/pspgbhu/Rephic.git',
-  desc: '支持微联本地调试的 vue 模板',
-}];
+module.exports = [
+  {
+    name: 'devtool-vue-template',
+    desc: '支持微联本地调试的 vue 模板',
+  },
+  {
+    name: 'aircleaner-JL3-template',
+    desc: '空气净化器3.0协议 H5',
+  },
+  {
+    name: 'airconditioner-JL3-template',
+    desc: '空调3.0协议 H5 模板',
+  },
+  {
+    name: 'airpurifier-JL3-template',
+    desc: '新风系统3.0协议 H5 模板',
+  },
+  {
+    name: 'curtain-JL3-template',
+    desc: '窗帘3.0协议 H5 模版',
+  },
+  {
+    name: 'dimmer-JL3-template',
+    desc: '调光面板3.0协议 H5 模版',
+  },
+  {
+    name: 'floorHeating-JL3-template',
+    desc: '地暖3.0协议 H5 模版',
+  },
+  {
+    name: 'panelSwitch-JL3-template',
+    desc: '多位开关3.0协议 H5 模版',
+  },
+  {
+    name: 'inductionLight-JL3-template',
+    desc: '人体感应灯3.0协议 H5 模版',
+  },
+  {
+    name: 'alarmbutton-JL3-template',
+    desc: '无线报警按钮3.0协议 H5 模版',
+  },
+  {
+    name: 'smartLock-JL3-template',
+    desc: '智能门锁3.0协议 H5 模版',
+  },
+  {
+    name: 'airMonitoring-JL3-template',
+    desc: '空气监测仪3.0协议 H5 模版',
+  },
+  {
+    name: '空气压力传感器3.0协议 H5 模版',
+    desc: 'airPressure-JL3-template',
+  },
+  {
+    name: '空调2.0协议 H5 模版',
+    desc: 'conditioning-JL2-template',
+  },
+  {
+    name: '冰箱控制 H5 模板',
+    desc: 'fridge-template',
+  },
+  {
+    name: '云端定时 H5 模板',
+    desc: 'timing-task-template',
+  },
+];
