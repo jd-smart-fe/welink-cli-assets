831da0d2313672d967340c62c89746d6bb13b881
tianmeng <tianmeng1@jd.com>
2019-01-11 15:34:25 +0800

    feat：删除 joylink 3.0

--- config/templates.js
+++ config/templates.js
@@ -12,55 +12,55 @@ module.exports = [
     name: 'devtool-vue-template',
     desc: '微联本地调试的 vue 模板',
   },
-  { type: 'separator', line: 'JoyLink 3.0' },
-  {
-    name: 'aircleaner-JL3-template',
-    desc: '空气净化器3.0协议 H5',
-  },
-  {
-    name: 'airconditioner-JL3-template',
-    desc: '空调3.0协议 H5 模板',
-  },
-  {
-    name: 'airpurifier-JL3-template',
-    desc: '新风系统3.0协议 H5 模板',
-  },
-  {
-    name: 'curtain-JL3-template',
-    desc: '窗帘3.0协议 H5 模版',
-  },
-  {
-    name: 'dimmer-JL3-template',
-    desc: '调光面板3.0协议 H5 模版',
-  },
-  {
-    name: 'floorHeating-JL3-template',
-    desc: '地暖3.0协议 H5 模版',
-  },
-  {
-    name: 'panelSwitch-JL3-template',
-    desc: '多位开关3.0协议 H5 模版',
-  },
-  {
-    name: 'inductionLight-JL3-template',
-    desc: '人体感应灯3.0协议 H5 模版',
-  },
-  {
-    name: 'alarmbutton-JL3-template',
-    desc: '无线报警按钮3.0协议 H5 模版',
-  },
-  {
-    name: 'smartLock-JL3-template',
-    desc: '智能门锁3.0协议 H5 模版',
-  },
-  {
-    name: 'airMonitoring-JL3-template',
-    desc: '空气监测仪3.0协议 H5 模版',
-  },
-  {
-    name: 'airPressure-JL3-template',
-    desc: '空气压力传感器3.0协议 H5 模版',
-  },
+  // { type: 'separator', line: 'JoyLink 3.0' },
+  // {
+  //   name: 'aircleaner-JL3-template',
+  //   desc: '空气净化器3.0协议 H5',
+  // },
+  // {
+  //   name: 'airconditioner-JL3-template',
+  //   desc: '空调3.0协议 H5 模板',
+  // },
+  // {
+  //   name: 'airpurifier-JL3-template',
+  //   desc: '新风系统3.0协议 H5 模板',
+  // },
+  // {
+  //   name: 'curtain-JL3-template',
+  //   desc: '窗帘3.0协议 H5 模版',
+  // },
+  // {
+  //   name: 'dimmer-JL3-template',
+  //   desc: '调光面板3.0协议 H5 模版',
+  // },
+  // {
+  //   name: 'floorHeating-JL3-template',
+  //   desc: '地暖3.0协议 H5 模版',
+  // },
+  // {
+  //   name: 'panelSwitch-JL3-template',
+  //   desc: '多位开关3.0协议 H5 模版',
+  // },
+  // {
+  //   name: 'inductionLight-JL3-template',
+  //   desc: '人体感应灯3.0协议 H5 模版',
+  // },
+  // {
+  //   name: 'alarmbutton-JL3-template',
+  //   desc: '无线报警按钮3.0协议 H5 模版',
+  // },
+  // {
+  //   name: 'smartLock-JL3-template',
+  //   desc: '智能门锁3.0协议 H5 模版',
+  // },
+  // {
+  //   name: 'airMonitoring-JL3-template',
+  //   desc: '空气监测仪3.0协议 H5 模版',
+  // },
+  // {
+  //   name: 'airPressure-JL3-template',
+  //   desc: '空气压力传感器3.0协议 H5 模版',
+  // },
   { type: 'separator', line: 'JoyLink 2.0' },
   {
     name: 'conditioning-JL2-template',
